eff81a32caeb38969fdd084726852b954be55a8b
siva10116 <seeramsiva6@gmail.com>
2026-09-05 12:30:40 +0530

    Updates

---
+++ assets/swimlanesDiagram-VR7AAH4N-B-vDh83a.js
@@ -0,0 +1,8 @@
+import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./src-B6xuSHsQ.js";import"./chunk-DU6HZSFF-DLHJJ3ki.js";import"./chunk-75Z2AOVW-DYFibh4r.js";import"./chunk-P2QGCYS3-CBC2hvnt.js";import"./chunk-PWAF6VOD-BxnJ4ObH.js";import"./chunk-GMAD6QVW-CzYG_oMC.js";import"./chunk-4HAMMTFA-Crko3E_A.js";import"./chunk-GVQU2GXP-CjrkM96x.js";import"./index-B0srljQN.js";import"./chunk-F27PBJKO-D0mzyXST.js";import"./chunk-XXDRQBXY-DqAYeljN.js";import"./chunk-POPQ4Y6H-Dy6rIZu8.js";import{r as t,t as n}from"./chunk-SHT3W25Y-hc-XQQL9.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
